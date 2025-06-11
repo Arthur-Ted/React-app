65b183e039dcee37e284d6262093b277dbf5e06e
Arthur-Ted <arthurwilliamkisala@gmail.com>
2025-06-11 14:11:40 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
 dotenv.config();
 
 const authRoutes = require('./routes/auth');
@@ -11,6 +15,15 @@ const logsRoutes = require('./routes/logs');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ message: 'Invalid JSON' });
+  }
+  next();
+});
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
